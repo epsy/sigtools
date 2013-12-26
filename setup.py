38a94b058c503e832d7a803d7304cfd2b91cd6aa
Yann Kaiser <kaiser.yann@gmail.com>
2013-12-26 22:05:32 +0100

    Updates for setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,33 @@ from setuptools import setup
 import sys
 
 require_funcsigs = sys.version_info < (3,3)
-requires = ['six']
+requirements = ['six']
 
 if require_funcsigs:
-    requires.append('funcsigs>=0.4')
+    requirements.append('funcsigs>=0.4')
 
 setup(
     name='sigtools',
     version='0.1a2',
+    description="Utilities for working with 3.3's inspect.Signature objects.",
+    license='MIT',
     author='Yann Kaiser',
     author_email='kaiser.yann@gmail.com',
     url='http://sigtools.readthedocs.org/',
     packages=['sigtools'],
-    install_requires=requires,
+    install_requires=requirements,
     test_suite='sigtools.tests',
+    keywords='introspection signature',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     )
