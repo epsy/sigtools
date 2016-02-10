17ff1bc9d8ba686aec92626f7961f6f9db5115b3
Yann Kaiser <kaiser.yann@gmail.com>
2016-02-10 12:25:03 +0100

    Allow for some output to be silenced in pyflakes output

---
+++ run-pyflakes.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PAT=$(grep -Hrn --binary-files=without-match 'pyflakes: silence' $1 | sed -ze 's/\([^:]*:[^:]*:\)[^\n]*\n/\1|/g' -e 's/.$//')
+
+if [ -n "$PAT" ]
+then
+    OUT=$(python3.5 -m pyflakes $1 | grep -Pv "$PAT")
+    if [ -n "$OUT" ]; then
+        echo $OUT; false
+    else
+        true
+    fi
+else
+    python3.5 -m pyflakes $1
+fi
