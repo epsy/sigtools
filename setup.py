fc5b3e78aa03f19748e6d719e787beab0f5c6a9c
Yann Kaiser <kaiser.yann@gmail.com>
2018-06-05 09:43:34 +0200

    version 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sigtools',
-    version='2.0.1',
+    version='2.0.2',
     description="Utilities for working with inspect.Signature objects.",
     license='MIT',
     author='Yann Kaiser',
